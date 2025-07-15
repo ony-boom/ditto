ebb24f9e3fc319d170726ec8c9f35dd85ed51e48
Ony <onyrakoto27@gmail.com>
2025-07-15 16:50:11 +0300

    added simple nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Ditto, manage arch package declaratively";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = {
+          default = pkgs.buildGoModule {
+            src = self;
+            pname = "ditto";
+            version = "0.1.0";
+            vendorHash = "sha256-AM4ZdnMRSBDEqqMDFtIkMAkaaSWkaEjZdV6mFsSH9y8=";
+          };
+        };
+      }
+    );
+}
